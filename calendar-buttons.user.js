f541dbd12694fd743e8ad7662c12fa1ffbe82284
Cecile Ledoux <ccl.ledoux@gmail.com>
2016-07-18 20:31:17 +0200

    -

--- calendar-buttons.user.js
+++ calendar-buttons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Buttons calendar
 // @namespace    TVMaze
-// @version      0.1
+// @version      0.1.1
 // @author       cicelle
 // @include      http://www.tvmaze.com/calendar*
 // @downloadURL  https://raw.githubusercontent.com/cicelle/tvmaze/master/calendar-buttons.user.js
