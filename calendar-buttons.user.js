245d29013f992076689070a47e77046d16ae9048
Cecile Ledoux <ccl.ledoux@gmail.com>
2016-07-18 20:05:38 +0200

    position jump to today

--- calendar-buttons.user.js
+++ calendar-buttons.user.js
@@ -9,8 +9,14 @@
 // ==/UserScript==
 (function() {
     $('head').append('<style>li p{margin-bottom: 0; margin-top: 5px; display: flex; justify-content: space-between;}'+
-                     'li p span{cursor:pointer; padding: 3px;}'+
+                     'li p span{cursor:pointer; padding: 2px;}'+
                      'li.watched p span[data-type="0"],li.acquired p span[data-type="1"]{background-color: #000; color: #fff; cursor: default;}'+
+                     '@media screen and (max-width: 600px) {'+
+                     'li{position: relative;}'+
+                     'li p {float: right; margin-top: 0px;}'+
+                     'li p span{margin:0 10px;}'+
+                     'li.entry:after{content:"";display:block;height:0;overflow:hidden;clear:both;}'+
+                     '}'+
                      '</style>');
     $('.episodes li').each(function(){
         $(this).append('<p><span data-type="2" class="buttons"><i class="fa fa-ban"></i></span>'+
@@ -18,6 +24,10 @@
             '<span data-type="1" class="buttons"><i class="fa fa-cloud"></i></span>'+
             '<span data-type="0" class="buttons"><i class="fa fa-eye"></i></span></p>');
     });
+    if($('.today').length > 0){
+        console.log($('.today').offset().top);
+        window.scrollTo(0, $('.today').offset().top);
+    }
     $('body').on('click', '.buttons', function(){
         var li = $(this).parent().parent();
         var type = $(this).attr('data-type');
