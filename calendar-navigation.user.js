ded3ef7eda1b61a88ccfda1c9d99b9d5d53a3a6e
cecile <ccl.ledoux@gmail.com>
2022-06-11 09:52:54 +0200

    Create calendar-navigation.user.js

---
+++ calendar-navigation.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Calendar navigation
+// @namespace    TVMaze
+// @version      1.0
+// @description  Navigation buttons
+// @author       cicelle
+// @include      http://www.tvmaze.com/calendar*
+// @include      https://www.tvmaze.com/calendar*
+// @downloadURL  https://raw.githubusercontent.com/cicelle/tvmaze/master/calendar-navigation.user.js
+// @license      The MIT License (MIT)
+// ==/UserScript==
+(function() {
+    'use strict';
+    $('head').append(`<style>
+                     #calendar-footer{background-color: #ebebeb; padding: 0.5rem 0;}
+                     #calendar-footer #month{font-size: 1.3rem;font-family: open sans,sans-serif;font-weight: 300}
+                     </style>`);
+
+    /****************************
+    add another calendar navigation
+    *****************************/
+    if($('#calendar-header').length){
+        let calfooter = '<article id="calendar-footer" class="grid-x align-middle align-center">'+$('#calendar-header').html()+'</article>';
+        $('#calendar-wrap .card').append(calfooter)
+    }
+})();
